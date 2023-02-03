6cd2ee88025c189cbacac62d6ab5c2d7c94302be
Eamonked <kendrickmon@gmail.com>
2023-02-03 05:44:52 +0400

    Update validationOfAnEmailID.java

--- EmailValidation/src/Assignments/validationOfAnEmailID.java
+++ EmailValidation/src/Assignments/validationOfAnEmailID.java
@@ -12,7 +12,7 @@ public class validationOfAnEmailID {
 
 		
 	List<String> emailId = new LinkedList<>();
-			
+		//Adding emails to the List	
 			emailId.add("xyz@gmail.com");
 			emailId.add("abc@gmail.com");
 			emailId.add("qwerty@gmail.com");
@@ -27,24 +27,28 @@ public class validationOfAnEmailID {
 			emailId.add("xyz@yahoo.com");
 			emailId.add("nmh@yahoo.com");
 	
-			
+			//A while loop to keep the code running.
 			while (true) {
-
+				//Requesting User Input for the email to check
 			System.out.println("Please Enter you Email Address:: \n");
 			Scanner StringValue = new Scanner(System.in);
 			
+			//variable to store the email entered by the user.
 			String Val1 = StringValue.next();
 			
 
 			System.out.println(emailId.size());
 			
-			
+			//Checking the emailID list for the email entered by the user
 				if (emailId.contains(Val1)){
 					System.out.println("Email:: " + Val1 + " Exists");
 				}
 				else {
 					System.out.println("Email:: " + Val1 + " Does Not Exist \n");
+					
+					//Giving the User an option to add the email entered to the list when its not found.
 					System.out.println("to add the email entered, press : 1  OR \n to cancel Press : 0 \n");
+									
 					int Val2 = StringValue.nextInt();
 					if (Val2 == 1) {
 						emailId.add(Val1);
